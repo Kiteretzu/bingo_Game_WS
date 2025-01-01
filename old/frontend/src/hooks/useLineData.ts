4382da10987d18df95127549a92e333bf18af31e
SmarthVerma <157901120+SmarthVerma@users.noreply.github.com>
2025-01-02 05:02:09 +0530

    Delete old directory

--- old/frontend/src/hooks/useLineData.ts
+++
@@ -1,9 +0,0 @@
-import  { useState } from "react";
-
-function useLineData() {
-  const [lineData, setLineData] = useState([["a", "b", "c", "d", "e"]]);
-
-  return { lineData, setLineData };
-}
-
-export default useLineData;
